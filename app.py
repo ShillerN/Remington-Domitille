b04afc6078db7640d60aafa95e01d34febbf538d
ShillerN <96434860+ShillerN@users.noreply.github.com>
2024-02-18 01:55:37 +0200

    Summary

---
+++ app.py
@@ -0,0 +1,163 @@
+import requests
+from pathlib import Path
+import streamlit as st
+from streamlit_lottie import st_lottie
+from PIL import Image
+import os
+import json
+
+# Directories and file paths
+THIS_DIR = Path(__file__).parent
+ASSETS = THIS_DIR / "images"
+LOTTIE_ANIMATION = ASSETS / "Animation - 1708211828248.json"
+
+
+# Function to load and display the Lottie animation
+def load_lottie_animation(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
+
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Main Reporting", page_icon=":📂:", layout="wide")
+
+
+# ---- LOAD ASSETS ----
+lottie_animation = load_lottie_animation(LOTTIE_ANIMATION)
+digest = Image.open("images/digest.png")
+payments = Image.open("images/Payments.png")
+som = Image.open("images/som.png")
+av = Image.open("images/av.png")
+bonds = Image.open("images/bonds.png")
+avd = Image.open("images/avd.png")
+   
+# ---- SUBHEADER----
+with st.container():
+    #st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.title("Reporting 📂")
+        st.write("##")      
+        st.write(
+            """
+           Below you might find main links:
+            """
+        )    
+        st.write("[Ukraine_Global_BI_PRD >](https://app.powerbi.com/groups/48aaa182-efc4-40eb-8e6f-ac264bdaea3e/list?openReportSource=ReportInvitation&ctid=8b86a65e-3c3a-4406-8ac3-19a6b5cc52bc&bookmarkGuid=Bookmark64ec64b10eb90492f0fb&experience=power-bi)")
+        st.write("[CIP portal >](https://commercialintelligenceportal-pmicloud.msappproxy.net/welcome)")
+    with right_column:
+        st.write("##")
+        st_lottie(lottie_animation, key="lottie-holiday", height=300)
+        
+        
+        
+# ---- Reports ----
+with st.container():
+    st.write("---")
+    st.header("Power BI & Excel reports")
+    st.write("##")
+    image_column, text_column = st.columns((1, 2 ))
+    with image_column:
+        st.image(digest)
+    with text_column:
+        st.subheader("Digest")
+        st.write(
+            """
+            Weekly IMS and SoM Dynamics by Area, Region, main customer group : NKA, SWH and oth. 
+            \nIncludes Device offtakes and Brand Retail performance. 
+            """
+        )
+        st.write("##")
+        st.write('Weekly updating')
+        st.markdown("[Digest>](https://app.powerbi.com/groups/48aaa182-efc4-40eb-8e6f-ac264bdaea3e/reports/416c1745-f13f-4dad-832c-fafa88a9c70d/ReportSection98cbde60b6649ede16a2?openReportSource=ReportInvitation&ctid=8b86a65e-3c3a-4406-8ac3-19a6b5cc52bc&experience=power-bi)")
+    
+ 
+with st.container():
+    st.write("##")
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(payments)
+    with text_column:
+        st.subheader("Shipments & Payments")
+        st.write(
+            """
+            Report includes customer balance, payments shedule and shipments. 
+            
+            """
+        )
+        st.write("##")
+        st.write('Daily updating')
+        st.markdown("[Shipments & Payments>](https://app.powerbi.com/groups/9a9e5d6e-bd9e-4df3-989d-2bc52f764079/reports/e7f1a8a6-5c23-451a-a548-2255754b25d7/ReportSection2f2179ab19034011e310?openReportSource=ReportInvitation&ctid=8b86a65e-3c3a-4406-8ac3-19a6b5cc52bc&experience=power-bi)")
+                
+ 
+with st.container():
+    st.write("##")
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(som)
+    with text_column:
+        st.subheader("SoM By Clients")
+        st.write(
+            """   
+            """
+        )
+        st.write("##")
+        st.write('Weekly updating')
+        st.markdown("[SoM By Clients>](https://pmicloud-my.sharepoint.com/:x:/r/personal/vmarchuk_pmintl_net/Documents/IMS/VolumePerformance/01_SoM%20By%20Clients.xlsx?d=wf4a08fce5bf1452cbb85897d449a5ee4&csf=1&web=1&e=DEouUr)")
+ 
+                
+with st.container():
+    st.write("##")
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(av)
+    with text_column:
+        st.subheader("Availability")
+        st.write(
+            """
+            
+            
+            """
+        )
+        st.write("##")
+        st.write('Daily updating')
+        st.markdown("[Availability>](https://pmicloud-my.sharepoint.com/:x:/r/personal/vmarchuk_pmintl_net/Documents/Desktop/Availability/AvailabilityReport.xlsx?d=w34d8493c8beb4e63a4812dfed77e3dc1&csf=1&web=1&e=OrZ65U)")
+   
+   
+with st.container():
+    st.write("##")
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(avd)
+    with text_column:
+        st.subheader("Availability with Distributor")
+        st.write(
+            """
+            
+            
+            """
+        )
+        st.write("##")
+        st.write('Daily updating')
+        st.markdown("[Availability with Distributor>](https://pmicloud-my.sharepoint.com/:x:/r/personal/vmarchuk_pmintl_net/Documents/Desktop/Availability/Availability_with_Distr.xlsx?d=w8d4ed320a1274e26bf59c4841b603949&csf=1&web=1&e=8mzrAR)")
+   
+with st.container():
+    st.write("##")
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(bonds)
+    with text_column:
+        st.subheader("Bonds & Blends")
+        st.write(
+            """
+            
+            
+            """
+        )
+        st.write("##")
+        st.write('Daily updating')
+        st.markdown("[Bonds & Blends>](https://pmicloud-my.sharepoint.com/:x:/r/personal/aradchuk_pmintl_net/Documents/Tasks/Reporting/BONDS%20%26%20Blends.xlsx?d=wf6082683223447038b4d05b75fd43697&csf=1&web=1&e=XhRiEi)")
+   
+ 
+ 
+##python -m streamlit run app.py
